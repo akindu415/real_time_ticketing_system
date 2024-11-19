bdc82761bf10137293e40128eedeffd82afe0cd6
akindu415 <akindu415@gmail.com>
2024-11-20 00:53:02 +0530

    create configuration class, main class
    to configure everything and main  class as ticketapp java to run the app

---
+++ src/Configuration.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Configuration {
+    private int totalTickets;
+    private int ticketReleaseRate;
+    private int ticketsPerRelease;
+    private int customerRetirevalRate;
+    private int maxTicketCapacity;
+    private int numberOfVendors;
+    private int numberOfCustomers;
+
+    public  void loadFromInput(Scanner scanner){
+        
+    }
+}
